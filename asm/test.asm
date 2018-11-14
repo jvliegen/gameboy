1a4eb2bd4e22d6900e58f00614bcc7ffd43486bd
Jo Vliegen <jo.vliegen@esat.kuleuven.be>
2018-11-15 00:13:35 +0100

    adding a compilable example

--- asm/test.asm
+++ asm/test.asm
@@ -1,4 +1,28 @@
-LD A, x0
-LD B, x1
-LD C, x2
-ADD A, B
+* this is comment for the assembler
+      ; this is comment as well
+;
+
+SECTION "start", HOME[$0100]
+nop
+jp begin
+
+; main: 
+;   jp $150
+;   ld a, 10
+;   ld b, 11
+;   ld c, 12
+;   ld d, 13
+;   ld e, 14
+;   ld h, 30
+;   ld l, AA
+;   ld a, b
+;   ld b, c
+;   ld c, d
+;   ld d, e
+;   ld e, h
+;   ld h, i
+;   ld i, a
+
+begin:
+  nop
+  di
